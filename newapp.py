f16b2508564faccfb90fecdf3bb3bf74f09a59ee
Ruben Cardoso <62020876+rubencardoso1@users.noreply.github.com>
2021-09-29 09:44:06 +0100

    Add files via upload

---
+++ newapp.py
@@ -0,0 +1,95 @@
+from docx import Document
+from docx.shared import Inches
+import pyttsx3
+
+
+def speak(text):
+    pyttsx3.speak(text)
+
+document = Document()
+
+# profile picture
+document.add_picture('me.jpg', width=Inches(2.0))
+
+
+#name phone number and email details
+speak('Hello there and welcome to the CV generator')
+name = input('What is your name? ')
+speak('Hello' + name + ', how are you today?')
+
+speak('What is your phone number?')
+phone_number = input('What is your phone number? ')
+
+speak('Thank you, and your email address?')
+email = input('What is your email address? ')
+
+document.add_paragraph(name + ' | ' + phone_number + ' | ' + email)
+
+
+# about me
+document.add_heading('About me')
+
+speak("That's great, now tell me about yourself?")
+document.add_paragraph(input('Tell about yourself? \n '))
+
+
+
+# Work experience
+speak('Do you have any work experience?')
+document.add_heading('Work Experience')
+p = document.add_paragraph()
+
+company = input('Enter company: \n ')
+from_date = input(' From Date: \n ')
+to_date = input('To Date: \n ')
+
+p.add_run(company + ' ').bold = True
+p.add_run(from_date + '-' + to_date + '\n' ).italic = True
+
+experience_details = input('Describe your experience at ' + company + ': \n ')
+p.add_run(experience_details)
+
+# more experiences
+
+while True:
+    has_more_experiences = input('Do you have more experiences? Yes or No \n ')
+    if has_more_experiences.lower() == 'yes':
+        p = document.add_paragraph()
+        
+        company = input('Enter company: ')
+        from_date = input(' From Date: ')
+        to_date = input('To Date: ')
+
+        p.add_run(company + ' ').bold = True
+        p.add_run(from_date + '-' + to_date + '\n' ).italic = True
+
+        experience_details = input('Describe your experience at ' + company + ': ')
+        p.add_run(experience_details)
+    else:
+        break
+
+# skills
+document.add_heading('Skills')
+skill = input('Enter skill: ' ) 
+p = document.add_paragraph(skill)
+p.style = 'List Bullet'
+
+while True:
+    has_more_skills = input('Do you have more skills? Yes or No: ')
+    if has_more_skills.lower() == 'yes':
+        skill = input('Enter skill: ')
+        p = document.add_paragraph(skill)
+        p.style = 'List Bullet'
+    else:
+        break
+
+
+# footer
+section = document.sections[0]
+footer = section.footer
+p = footer.paragraphs[0]
+p.text = "CV generated using Ruben's CV Generator"
+
+
+
+document.save('cv.docx')
